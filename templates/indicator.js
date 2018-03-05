3196b41e8b76b51026b25c6db940fd8147176c43
Hearty Oh <heartyoh@hatiolab.com>
2018-03-05 11:51:43 +0900

    fix template spec

--- templates/indicator.js
+++ templates/indicator.js
@@ -1,21 +1,16 @@
 export default {
-  name: 'indicator',
-  /* 다국어 키 표현을 어떻게.. */
-  description: '...',
-  /* 다국어 키 표현을 어떻게.. */
+  type: 'indicator',
+  description: 'Multi-purpose Indicator Component.',
   group: 'IoT',
   /* line|shape|textAndMedia|chartAndGauge|table|container|dataSource|IoT|3D|warehouse|form|etc */
-  icon: '../',
-  template: {
+  icon: '..',
+  model: {
     type: 'indicator',
-    model: {
-      type: 'indicator',
-      left: 10,
-      top: 10,
-      width: 449,
-      height: 53,
-      fillStyle: 'black',
-      strokeStyle: 'darkgray'
-    }
+    left: 10,
+    top: 10,
+    width: 449,
+    height: 53,
+    fillStyle: 'black',
+    strokeStyle: 'darkgray'
   }
 };
--- templates/seven-segment.js
+++ templates/seven-segment.js
@@ -1,33 +1,28 @@
 export default {
-  name: 'seven-segment',
-  /* 다국어 키 표현을 어떻게.. */
-  description: '...',
-  /* 다국어 키 표현을 어떻게.. */
+  type: 'seven-segment',
+  description: 'Electric Seven Segment Emulation Component',
   group: 'IoT',
   /* line|shape|textAndMedia|chartAndGauge|table|container|dataSource|IoT|3D|warehouse|form|etc */
-  icon: '../',
-  template: {
+  icon: '..',
+  model: {
     type: 'seven-segment',
-    model: {
-      type: 'seven-segment',
-      left: 10,
-      top: 10,
-      width: 200,
-      height: 300,
-      fillStyle: 'black',
-      strokeStyle: 'darkgray',
-      pattern: '##:##:##',
-      value: '12:34:56',
-      digitHeight: 20,
-      digitWidth: 10,
-      digitDistance: 2.5,
-      displayAngle: 12,
-      segmentWidth: 2.5,
-      segmentDistance: 0.2,
-      segmentCount: 7,
-      cornerType: 0,
-      colorOn: 'rgb(233, 93, 15)',
-      colorOff: 'rgb(75, 30, 5)'
-    }
+    left: 10,
+    top: 10,
+    width: 200,
+    height: 300,
+    fillStyle: 'black',
+    strokeStyle: 'darkgray',
+    pattern: '##:##:##',
+    value: '12:34:56',
+    digitHeight: 20,
+    digitWidth: 10,
+    digitDistance: 2.5,
+    displayAngle: 12,
+    segmentWidth: 2.5,
+    segmentDistance: 0.2,
+    segmentCount: 7,
+    cornerType: 0,
+    colorOn: 'rgb(233, 93, 15)',
+    colorOff: 'rgb(75, 30, 5)'
   }
 }
